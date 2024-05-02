00cf7850b12e7b21e8b1d0b72cb72bb8aa59b41f
Marcelllombm <melombm@hotmail.com>
2024-05-02 06:57:21 -0300

    style: adjust css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import "./Header.module.css";
+import style from  "./Header.module.css";
 import rocket from "../assets/rocket.svg";
 
 export  function Header() {
   return (
     
-    <header>
+    <header className={style.headerMain}>
       <div>
         <img src={rocket} alt="navi" />
         <h1><span>to</span>do</h1>
